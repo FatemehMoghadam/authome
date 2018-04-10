d7f532094e70b4c25a041d0c468cada7fa699a93
Scott Percival <scott.percival@dbca.wa.gov.au>
2018-04-10 17:02:00 +0800

    uwsgi: do migrations

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+python manage.py migrate --noinput
+
 # TODO: make PR for https://github.com/sclorg/s2i-python-container which auto-does this
 exec uwsgi --ini "$APP_CONFIG" --module "$APP_MODULE"
