aa6f0bb8d8fc91702adeff586358aff874c59cb4
Scott Percival <scott.percival@dbca.wa.gov.au>
2018-09-18 15:11:15 +0800

    update gevent, fix typo in views

--- authome/views.py
+++ authome/views.py
@@ -257,10 +257,10 @@ def auth(request):
     }
     response = HttpResponse(json.dumps(response_contents), content_type='application/json')
     headers = response_contents
-    headers["full_name"] = u" ".join(
+    headers["full_name"] = u" ".join([
         headers.get("first_name", ""),
         headers.get("last_name", ""), 
-    ).strip()
+    ]).strip()
     headers["logout_url"] = "/sso/auth_logout"
    
     # cache response
